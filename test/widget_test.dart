83db721b663c046b7752ddeec1b0f2d13fa82c31
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2026-01-01 18:20:19 +0545

    test: updated test descriptions and comments

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+// Widget tests for verifying app startup
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
